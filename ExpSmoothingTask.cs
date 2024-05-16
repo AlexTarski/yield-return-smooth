32bb6b4730c0750156c3ee9d8d9af35ba0d629a5
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-16 19:36:23 +0300

    Add Exponential smoothing feature

--- ExpSmoothingTask.cs
+++ ExpSmoothingTask.cs
@@ -4,9 +4,27 @@ namespace yield;
 
 public static class ExpSmoothingTask
 {
-	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
+    public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
 	{
-		//Fix me!
-		return data;
+        bool isFirstPoint = true;
+        double currentYPoint;
+        double previousYPoint = 0;
+
+        foreach (var point in data)
+		{
+            if (isFirstPoint)
+            {
+                previousYPoint = point.OriginalY;
+                currentYPoint = point.OriginalY;
+                isFirstPoint = false;
+            }
+            else
+            {
+                currentYPoint = alpha * point.OriginalY + (1 - alpha) * previousYPoint;
+                previousYPoint = currentYPoint;
+            }
+            DataPoint newPoint = point.WithExpSmoothedY(currentYPoint);
+            yield return newPoint;
+		}
 	}
 }
