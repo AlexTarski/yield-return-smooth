df54ec8452ec65887d741b23569963bcdb69eaec
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-16 20:17:48 +0300

    Add Moving average smoothing feature

--- MovingAverageTask.cs
+++ MovingAverageTask.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace yield;
 
@@ -6,7 +8,23 @@ public static class MovingAverageTask
 {
 	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		//Fix me!
-		return data;
+		Queue<double> previousYs= new();
+		double averageSum = 0;
+		foreach (DataPoint point in data)
+		{
+            Average(previousYs, windowWidth, point.OriginalY, ref averageSum);
+            DataPoint newPoint = point.WithAvgSmoothedY(averageSum / previousYs.Count);
+			yield return newPoint;
+        }
 	}
+
+	public static void Average(Queue<double> previousYs, int windowWidth, double currentY, ref double averageSum)
+	{
+        previousYs.Enqueue(currentY);
+        averageSum += currentY;
+        if (previousYs.Count > windowWidth)
+        {
+            averageSum -= previousYs.Dequeue();
+        }
+    }
 }
